665c5ab1e92e02dcfc92993bbf2e47fa917b1375
abidingulsun06 <71988251+abidingulsun06@users.noreply.github.com>
2022-09-22 15:09:45 +0300

    güncellendi

--- src/Ders2/Odev2/Methods.java
+++ src/Ders2/Odev2/Methods.java
@@ -1,9 +1,29 @@
 package Ders2.Odev2;
 
 public class Methods {
-
     public static void main(String[] args) {
-        System.out.println("Selam");
+        sayiBulma(9);
+    }
+    public static void sayiBulma(int par){
+        int[] sayilar = new int[]{1, 2, 5, 7, 9, 0};
+        int aranacak = par;
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+
+        if (varMi)
+            mesajVer("Sayı Mevcuttur:"+par);
+        else
+            mesajVer("Sayı mevcut değildir:"+ par);
+    }
+
+    public static void mesajVer(String message){
+        System.out.println(message);
     }
 
 }
