9281f2cc3a806542976065cd7c99d38b66d162c4
abidingulsun06 <71988251+abidingulsun06@users.noreply.github.com>
2022-10-07 23:51:18 +0300

    3.Ödev Bitt

--- src/Ders3/Odev3/business/KursManager.java
+++ src/Ders3/Odev3/business/KursManager.java
@@ -22,19 +22,19 @@ public class KursManager {
         this.kategoriler = kategoriler;
     }
 
-    public void addToCourse(ArrayList<Kurslar> courses ,Kategoriler[] kategorilers) throws Exception {
+    public void addToCourse(ArrayList<Kurslar> courses, Kategoriler[] kategorilers) throws Exception {
 
-        Kategoriler kategoriler1 = new Kategoriler(1,"Front end");
-        Kurslar javaScript = new Kurslar(3, "Yazılım Geliştirici Yetiştirme Kampı Javascript", 33,kategoriler1.getKategoriAdi());
+        Kategoriler kategoriler1 = new Kategoriler(1, "Front end");
+        Kurslar javaScript = new Kurslar(3, "Yazılım Geliştirici Yetiştirme Kampı Javascript", 33, kategoriler1.getKategoriAdi());
 
-        for(Kurslar k : courses){
+        for (Kurslar k : courses) {
             if (k.getKursAdi().equalsIgnoreCase(javaScript.getKursAdi()))
-                throw  new Exception("Ayni isimde 2 kurs bulunamaz");
-            else if (k.getKursFiyati() <= 0)
-                throw  new Exception("Kurs fiyatı 0 yada 0 dan küçük olamaz.");
+                throw new Exception("Ayni isimde 2 kurs bulunamaz");
+            else if (k.getKursFiyati() <= 0 || javaScript.getKursFiyati() <= 0)
+                throw new Exception("Kurs fiyatı 0 yada 0 dan küçük olamaz.");
         }
 
-        for(Kategoriler categories : kategorilers){
+        for (Kategoriler categories : kategorilers) {
             if (categories.getKategoriAdi().equalsIgnoreCase(kategoriler1.getKategoriAdi()))
                 throw new Exception("Ayni isimde 2 kategori olamaz");
         }
